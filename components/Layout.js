4792412d6a394fda8be9c59207756f44c2ee8650
Jason Pierce <jason.pierce.designs@gmail.com>
2020-04-20 15:35:52 -0400

    adjusting Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import JPtheme from '../public/JPtheme';
 import Header from './Header';
 import Navigation from './Navigation';
 import BackgroundLines from "../components/BackgroundLines";
+import { initGA, logPageView } from '../utils/analytics'
 
-export default ({ children, pageMetaData }) => (
-    <ThemeProvider theme={JPtheme}>
-        <div className="mi-wrapper">
-            <BackgroundLines />
-            <Header pageMetaData={pageMetaData}></Header>
-            <Navigation></Navigation>
-            {children}
-        </div>
-    </ThemeProvider>
-);
+export default ({ children, pageMetaData }) => {
+
+    useEffect(() => {
+        if (!window.GA_INITIALIZED) {
+            initGA();
+            window.GA_INITIALIZED = true
+          }
+          logPageView();
+    }, []);
+
+    return (
+        <ThemeProvider theme={JPtheme}>
+            <div className="mi-wrapper">
+                <BackgroundLines />
+                <Header pageMetaData={pageMetaData}></Header>
+                <Navigation></Navigation>
+                {children}
+            </div>
+        </ThemeProvider>
+    );
+};
